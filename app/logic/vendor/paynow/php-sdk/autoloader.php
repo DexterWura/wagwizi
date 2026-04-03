cfab547769f13f3d5a4b90cbabe6861c8b4f1945
Dexterity Wurayayi <dexterwurayayi@gmail.com>
2026-04-03 10:10:00 +0200

    paynow fixes

--- app/logic/vendor/paynow/php-sdk/autoloader.php
+++ app/logic/vendor/paynow/php-sdk/autoloader.php
@@ -15,22 +15,19 @@ define('ROOT_NAMESPACE', 'Paynow\\');
  * 
  * @link (GitHub Gist, https://gist.github.com/melmups/a0800b07e58089297c1735cfcc9fd382)
  */
-spl_autoload_register(function($class) {
-        // Remove the root namespace
-        if (substr($class, 0, strlen(ROOT_NAMESPACE)) == ROOT_NAMESPACE) {
-            $relative = substr($class, strlen(ROOT_NAMESPACE));
-        } 
-
-        // Bring in the file
-        $filename = __DIR__ . "/src/" . str_replace('\\', '/', $relative) . ".php";
-
-        // Check if the file exists
-        if (file_exists($filename)) {
-            require_once($filename);
-            if (class_exists($class)) {
-                return true;
-            }
-        }
-
+spl_autoload_register(function ($class) {
+    if (strncmp($class, ROOT_NAMESPACE, strlen(ROOT_NAMESPACE)) !== 0) {
         return false;
+    }
+
+    $relative = substr($class, strlen(ROOT_NAMESPACE));
+    $filename = __DIR__ . '/src/' . str_replace('\\', '/', $relative) . '.php';
+
+    if (! is_file($filename)) {
+        return false;
+    }
+
+    require_once $filename;
+
+    return class_exists($class, false);
 });
